f621e88f279a3469ed55109b70b5c61c1059983f
Mahelder Melo <mahelder.melo@luizalabs.com>
2021-01-22 17:50:01 -0300

    feat: filtro tela de voluntarios

--- src/views/Volunteers/VolunteersList.js
+++ src/views/Volunteers/VolunteersList.js
@@ -8,6 +8,13 @@ import CardBody from "components/Card/CardBody.js";
 import Button from "components/CustomButtons/Button.js";
 import { CircularProgress } from "@material-ui/core";
 
+import Input from "@material-ui/core/Input";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import FormControl from "@material-ui/core/FormControl";
+import Icon from "@material-ui/core/Icon";
+
+import slugify from "utils/slugify";
+
 import firebase from "firebase";
 
 class VolunteersList extends React.Component {
@@ -15,7 +22,9 @@ class VolunteersList extends React.Component {
     super(props);
     this.state = {
       volunteers: [],
+      volunteersShow: [],
     };
+    this.handleSearchChange = this.handleSearchChange.bind(this);
   }
 
   componentDidMount() {
@@ -52,7 +61,16 @@ class VolunteersList extends React.Component {
       ]);
     });
     volunteers = volunteers.sort((a, b) => a[0].localeCompare(b[0]));
-    this.setState({ volunteers });
+    this.setState({ volunteers, volunteersShow: volunteers });
+  }
+
+  handleSearchChange(event) {
+    let { volunteersShow, volunteers, search } = this.state;
+    search = event.target.value;
+    volunteersShow = volunteers.filter((x) =>
+      slugify(x[0].toLowerCase()).includes(slugify(search.toLowerCase()))
+    );
+    this.setState({ volunteersShow, search });
   }
 
   render() {
@@ -62,9 +80,22 @@ class VolunteersList extends React.Component {
           <Button type="button" color="info" onClick={this.handleNew}>
             Adicionar
           </Button>
+          <FormControl style={{ marginLeft: 20, marginTop: 15 }}>
+            <Input
+              id="standard-adornment-weight"
+              value={this.state.search}
+              onChange={this.handleSearchChange}
+              endAdornment={
+                <InputAdornment position="end">
+                  <Icon>search</Icon>
+                </InputAdornment>
+              }
+              aria-describedby="standard-weight-helper-text"
+            />
+          </FormControl>
           <Card>
             <CardBody>
-              {this.state.volunteers.length > 0 && (
+              {this.state.volunteersShow.length > 0 && (
                 <Table
                   tableHeaderColor="primary"
                   tableHead={[
@@ -74,10 +105,16 @@ class VolunteersList extends React.Component {
                     "Comum Congregação",
                     "",
                   ]}
-                  tableData={this.state.volunteers}
+                  tableData={this.state.volunteersShow}
                 />
               )}
-              {this.state.volunteers.length === 0 && <CircularProgress />}
+              {this.state.volunteersShow.length === 0 &&
+                this.state.volunteers.length === 0 && <CircularProgress />}
+
+              {this.state.volunteersShow.length === 0 &&
+                this.state.volunteers.length > 0 && (
+                  <p>Sem voluntários cadastrados</p>
+                )}
             </CardBody>
           </Card>
         </GridItem>
